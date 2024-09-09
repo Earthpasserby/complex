b666bc78d04d0cba0f84a29dfdaa352bd8e95c8d
Earthpasserby <iwuno68@gmail.com>
2024-09-09 12:58:00 +0100

    header

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="container">
+          <nav>
+               
+          </nav>
+      </div>
+    </>
+  );
+};
+export default Navbar;
