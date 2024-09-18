e538d78beae0686ff7a7e8bb1d1b1a5a80aacb89
Earthpasserby <iwuno68@gmail.com>
2024-09-18 10:50:41 +0100

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,21 +3,54 @@
 const Navbar = () => {
   return (
     <>
-      <div className="container">
-        <nav>
+      <header className="mb-2 shadow px-4">
+        <div className="container mx-auto flex max-w-screen-lg items-center pt-5">
           <div className="w-32">
             <img src="pro.png" alt="prodev-logo" />
           </div>
-          <ul>
-            <li>Clients</li>
-            <li>Why Africa?</li>
-            <li>Talents</li>
-            <li>Products</li>
-            <li>Services</li>
-            <li>Pricing</li>
-          </ul>
-        </nav>
-      </div>
+          <nav>
+            <ul className="flex md:gap-x-8">
+              <li>
+                <a href="#np">Clients</a>
+              </li>
+              <li>
+                {" "}
+                <a href="#np">Why Africa?</a>
+              </li>
+              <li>
+                {" "}
+                <a href="#np">Talents</a>
+              </li>
+              <li>
+                {" "}
+                <a href="#np">Products</a>
+              </li>
+              <li>
+                {" "}
+                <a href="#np">Services</a>
+              </li>
+              <li>
+                {" "}
+                <a href="#np">Pricing</a>
+              </li>
+              <ul className="flex md:gap-x-8">
+                <li>
+                  <a href="#np">Talk to us</a>
+                </li>
+                <li>
+                  <a href="#np">Apply as a talent</a>
+                </li>
+                <li>
+                  {" "}
+                  <li>
+                    <a href="#np">Login</a>
+                  </li>
+                </li>
+              </ul>
+            </ul>
+          </nav>
+        </div>
+      </header>
     </>
   );
 };
